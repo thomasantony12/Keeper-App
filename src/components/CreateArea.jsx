93bb3c1407b87352d2e3051f16372d85262eeb21
Thomas antony <thomasantony12042001@gmail.com>
2024-09-23 14:24:14 +0530

    added new UI
    added  some new transitions and textures

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
+  const [isOn, setIsOn] = useState(false);
   const [note, setNote] = useState({
     title: "",
-    content: ""
+    content: "",
   });
 
   function handleChange(event) {
     const { name, value } = event.target;
 
-    setNote(prevNote => {
+    setNote((prevNote) => {
       return {
         ...prevNote,
-        [name]: value
+        [name]: value,
       };
     });
   }
@@ -21,28 +25,44 @@ function CreateArea(props) {
     props.onAdd(note);
     setNote({
       title: "",
-      content: ""
+      content: "",
     });
     event.preventDefault();
   }
 
+  function mouseOverHandler() {
+    setIsOn(true);
+  }
+
+  function mouseOutHandler() {
+    setIsOn(false);
+  }
+
   return (
     <div>
-      <form>
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
+      <form className="create-note">
+        {isOn && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
         <textarea
+        onClick={mouseOverHandler}
+          visibility={isOn ? "visible" : "hidden"}
           name="content"
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          rows={isOn ? "3" : "1"}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={isOn}>
+        <Fab onClick={submitNote}>
+          <AddIcon onClick={mouseOutHandler}/>
+        </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import EditNotificationsIcon from '@mui/icons-material/EditNotifications';
 
 function Header() {
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1><EditNotificationsIcon /> Keeper</h1>
     </header>
   );
 }
